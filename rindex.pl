34ce2acd8550d432c6380cd086901f24cc832252
Ben Crudo <ben.crudo@gmail.com>
2011-09-14 01:36:22 -0400

    Got a basic dictionary going. Needs some refinement but a nice start.

--- rindex.pl
+++ rindex.pl
@@ -6,11 +6,46 @@ use warnings;
 $file = "/media/320/Users/Ben/School/Concordia University/Classes/COMP 479 (Information Retrieval)/Class notes Sept 7 2011.txt";
 
 open("IN", "<".$file) or die ("File " .$file." - Not Found.\n");
-
-$line_counter = 0;
+# $line_counter = 0;
+%index = (); # make a hash the dictionary that will store an array or documents
+@tokens = (); # Store the tokens in an array for processing
 
 while ($line = readline(IN))
-{
-  print $line."\n";
+{ 
+  # remove the new line character
+  chomp($line);
+  # split the contents of each line on spaces into tokens
+  # side effect here is compound words stay attached
+  @tokens = split(/\s/, $line);
+
+  foreach(@tokens)
+  {
+     # Remove , : ; ! ? \ / ( ) < > [ ] { } _ * = 
+    $_ =~ s/,*:*;*#*~*`*\$*%*\**\(*\)*=*{*}*\[*\]*<*>*_*\\*\/*\|*//g;
+
+    if ($_ =~ m/^-+$/) 
+    {
+      # if the current token is a dash, do nothing
+    }
+    elsif ($_ =~ m/\d+/)
+    {
+      # if the current token is a number, do nothing
+    }
+    else
+    {
+      # the word is valid, store it in the index in lower case
+      @postings = ();
+      push(@postings, $file);
+      push @{ $index{lc($_)} }, \@postings;
+    }
+  } 
 }
+
 close IN;
+
+while(($key, $value) = each(%index))
+{
+  print "$key\n";
+}
+
+#@tokens = grep { /[A-Z][a-z]/, $_ } @tokens;
